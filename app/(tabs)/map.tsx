1fe5567568bc7b338e144829b0fe61379c013859
emreunlu <unlcemre0@gmail.com>
2024-04-06 04:10:48 +0300

    mapview bug fied

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,50 +1,64 @@
-import { View, Text, ScrollView } from "react-native";
-import React, { useEffect, useRef } from "react";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useStationRoutes } from "@/store/features/stationRoutes";
-import Map, {
-  Callout,
-  Marker,
-  Polygon,
-  Polyline,
-  PROVIDER_GOOGLE,
-} from "react-native-maps";
+import Map, { Marker, Polyline, PROVIDER_GOOGLE } from "react-native-maps";
 import colors from "tailwindcss/colors";
+import { useFocusEffect, useNavigation } from "expo-router";
 
 export default function MapView() {
   const { stations } = useStationRoutes();
+  const navigation = useNavigation();
 
   const mapRef = useRef<Map | null>(null);
 
-  useEffect(() => {
-    /* if (stations && stations.length > 0) {
-      mapRef.current?.animateToRegion({
-        latitude: parseFloat(stations[0].Enlem),
-        longitude: parseFloat(stations[0].Boylam),
-        latitudeDelta: 0.3,
-        longitudeDelta: 0.3,
-      });
-    } */
+  useFocusEffect(
+    useCallback(() => {
+      if (stations && stations.length > 0) {
+        mapRef.current?.setCamera({
+          center: {
+            latitude: parseFloat(stations[0].Enlem),
+            longitude: parseFloat(stations[0].Boylam),
+          },
+          zoom: 12,
+        });
+      }
+    }, [stations])
+  );
 
-    console.log("stations changed");
-  }, [stations]);
+  if (!stations)
+    return (
+      <View className="flex-1 items-center justify-center flex-col space-y-6">
+        <Text className="font-medium text-xl text-zinc-600">
+          İstasyon Bilgisi Bulunamadı!
+        </Text>
 
-  if (!stations) return null;
+        <TouchableOpacity
+          className="bg-green-500 px-8 py-3 rounded-full"
+          onPress={() => navigation.goBack()}
+        >
+          <Text className="text-white font-semibold">İstasyon Seç</Text>
+        </TouchableOpacity>
+      </View>
+    );
 
   return (
     <View className="flex-1">
       <Map
         provider={PROVIDER_GOOGLE}
         ref={mapRef}
+        zoomEnabled={false}
         style={{
           alignSelf: "stretch",
           height: "100%",
         }}
       >
         <Polyline
-          coordinates={stations.map((station) => ({
-            latitude: parseFloat(station.Enlem),
-            longitude: parseFloat(station.Boylam),
-          }))}
+          coordinates={[...stations]
+            .sort((a, b) => a.IstasyonSirasi - b.IstasyonSirasi)
+            .map((station) => ({
+              latitude: parseFloat(station.Enlem),
+              longitude: parseFloat(station.Boylam),
+            }))}
           strokeColor={colors.rose[500]}
           strokeWidth={5}
         />
